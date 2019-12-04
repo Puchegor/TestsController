4d8257d53f8c4a2ec42304ae025aeb87bf29555c
Puchegor <diz@tut.by>
2019-12-04 10:56:31 +0300

    12.11.19

--- src/sample/Controllers/AddTaskToTest.java
+++ src/sample/Controllers/AddTaskToTest.java
@@ -5,8 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import sample.Classes.Alerts;
@@ -24,14 +23,23 @@ public class AddTaskToTest implements Initializable {
     @FXML
     Button btnOk, btnCancel;
     @FXML
-    ListView listView;
+    ListView listView, lvToTest;
     @FXML
     AnchorPane apRight;
+    @FXML
+    Label labTaskNum;
+    @FXML
+            CheckBox cbRemoveTasks;
+
+    int taskNo = 1;
+    ObservableList<Task> tasks = FXCollections.observableArrayList();
+    ObservableList<Task> activeTasks = FXCollections.observableArrayList();
+    ArrayList<ObservableList<Task>> toTest = new ArrayList<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ArrayList<Integer> topicIDs = TestData.getTopicIDs();
-        ObservableList<Task> tasks = FXCollections.observableArrayList();
+
         int numTasks = TestData.getNumTasks();
         ResultSet rst;
         for (int id: topicIDs){
@@ -44,13 +52,8 @@ public class AddTaskToTest implements Initializable {
             }
         }
         listView.setItems(tasks);
-
-        //------------------------------
-        for (int i = 0; i < numTasks; i++){
-            ListView<Task> lv = new ListView<>();
-            apRight.getChildren().add(lv);
-        }
-        //------------------------------
+        taskNo = 1;
+        labTaskNum.setText("Задача № "+taskNo);
     }
 
     public void onCancelHandle(ActionEvent actionEvent) {
@@ -59,5 +62,51 @@ public class AddTaskToTest implements Initializable {
     }
 
     public void onOkHandle(ActionEvent actionEvent) {
+    }
+
+    public void onToTestHandle(ActionEvent actionEvent) {
+        MultipleSelectionModel<Task> selection = listView.getSelectionModel();
+        selection.setSelectionMode(SelectionMode.SINGLE);
+        activeTasks.add(selection.getSelectedItem());
+        lvToTest.setItems(activeTasks);
+        if (cbRemoveTasks.isSelected()){
+            tasks.remove(selection.getSelectedItem());
+        }
+    }
+
+    public void onOutTestHandle(ActionEvent actionEvent) {
+        MultipleSelectionModel<Task> selection = lvToTest.getSelectionModel();
+        selection.setSelectionMode(SelectionMode.SINGLE);
+        activeTasks.remove(selection.getSelectedItem());
+        if (cbRemoveTasks.isSelected())
+            tasks.add(selection.getSelectedItem());
+    }
+
+    public void onNextHandle(ActionEvent actionEvent) {
+        if (toTest.size()==0)
+            toTest.add(taskNo, lvToTest.getItems());
+        /*if (toTest.size() == 0 || toTest.get(taskNo)==null)
+            toTest.add(taskNo, lvToTest.getItems());
+        else
+            toTest.set(taskNo, lvToTest.getItems());
+        if (taskNo < TestData.getNumTasks()){
+            taskNo = ++taskNo;
+            labTaskNum.setText("Задача № "+taskNo);
+        } else {
+            taskNo = 1;
+            labTaskNum.setText("Задача № "+taskNo);
+        }
+        if (toTest.get(taskNo)!=null)
+            lvToTest.setItems(toTest.get(taskNo));*/
+    }
+
+    public void onPrevHandle(ActionEvent actionEvent) {
+        /*if (taskNo > 1 && taskNo <=TestData.getNumTasks()){
+            taskNo = --taskNo;
+            labTaskNum.setText("Задача № "+taskNo);
+        }else {
+            taskNo = TestData.getNumTasks();
+            labTaskNum.setText("Задача №"+taskNo);
+        }*/
     }
 }
